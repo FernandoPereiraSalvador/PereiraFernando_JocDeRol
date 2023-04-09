8683f532ad92b42ea6bcaf5e5ea913c9c48f543c
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-04-09 22:35:13 +0200

    Ficheros añadidos
    Se han añadido los siguientes ficheros:
    - Ficheros de teclat.
    - Ficheros de inici.

--- src/main/java/com/mycompany/pereirafernando_jocderol/PereiraFernando_JocDeRol.java
+++ src/main/java/com/mycompany/pereirafernando_jocderol/PereiraFernando_JocDeRol.java
@@ -12,6 +12,6 @@ package com.mycompany.pereirafernando_jocderol;
 public class PereiraFernando_JocDeRol {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello");
     }
 }
